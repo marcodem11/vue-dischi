c68c904b9493d6103151f9e507801be2d40abc75
marcodem11 <marcodemicheleta01@outlook.it>
2022-03-11 10:32:39 +0100

    Header.vue

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="my-header">
+    <div class="logo">
+      <img src="../assets/spotify-logo.png" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+#my-header{
+  width: 100vw;
+  background-color: #2E3A46;
+  min-height: 70px;
+  display: flex;
+  align-items: center;
+}
+.logo{
+  width: 50px;
+  margin-left: 20px;
+}
+</style>
