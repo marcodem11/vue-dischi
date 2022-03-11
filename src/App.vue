62ad5d7a9211a423fc75cfd787d8644a44907b34
marcodem11 <marcodemicheleta01@outlook.it>
2022-03-11 10:33:39 +0100

    app.vue

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <MyHeader />
+    <MyMain />
+  </div>
+</template>
+
+<script>
+import MyHeader from "./components/MyHeader.vue";
+import MyMain from "./components/MyMain.vue";
+export default {
+  name: "App",
+  components: {
+    MyHeader,
+    MyMain,
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+img {
+  max-width: 100%;
+}
+#app {
+  font-family: Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: #1e2d3b;
+  height: 100vh;
+}
+</style>
